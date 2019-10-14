14b3ef80335c755eb6e8db421086def14db315fc
ShafoYoisho <muhammadsyafizuraimi@Gmail.com>
2019-10-14 18:04:52 +0800

    msg

--- db.js
+++ db.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
 
+let dbUrl;
+if (process.env.NODE_ENV === "development") {
+  const dbName = "kittens-db";
+  dbUrl = `mongodb://localhost/${dbName}`;
+}
+
+if (process.env.NODE_ENV === "production") {
+  dbUrl = process.env.MONGO_URI;
+}
+
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
 
-const dbName = "kittens-db";
-let dbUrl;
-if (process.env.NODE_ENV === "development") {
-  dbUrl = `mongodb://localhost/${dbName}`;
-}
 mongoose.connect(dbUrl);
-
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
--- index.js
+++ index.js
@@ -2,9 +2,15 @@ require("dotenv").config();
 const app = require("./app");
 const env = app.get("env");
 
-if (app.get("env") === "development") {
-  PORT = 5000;
-}
+// if (app.get("env") === "development") {
+//   PORT = 5000;
+// }
+
+// if(app.get("env") === "production"){
+//   port = port.process.PORT
+// }
+
+const PORT = port.process.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`App is now listening in Port ${PORT} in ${env} mode`);
